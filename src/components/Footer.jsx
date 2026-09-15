21d42cf6fba2c55d2bc43e0d1cfed21120d5ac2a
haidar <haidarabas@gmail.com>
2026-09-15 22:54:58 +0800

    feat: add credits to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,41 @@
+import { projectCredits } from "../projectCredits";
+
+
 export default function Footer() {
   return (
     <footer className="footer" >
-      <p>I am footer</p>
+      <p>Designed by <a id="gitLink">Haidar Abas</a></p>
+      <Credits />
     </footer>
+  )
+}
+
+function Credits() {
+
+  const handleClickShowModal = () => {
+    document.getElementById('creditsModal').showModal();
+  }
+
+  return (
+    <>
+      <p id="credits" onClick={handleClickShowModal}>Credits</p>
+      <dialog id="creditsModal" closedby="any">
+        Credits:
+        <ul>
+          {
+            projectCredits.map((source) => {
+              return (
+                <li key={source.name}>
+                  <p>
+                    {source.desc}
+                    <a href={source.link} target="_blank"><img src="src/assets/open-in-new.svg" alt="link" /></a>
+                  </p>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </dialog>
+    </>
   )
 }
---
+++ src/projectCredits.js
@@ -0,0 +1,7 @@
+const projectCredits = [
+  { name: 'api', provider: 'Iconify', desc: 'Api provided by Iconify', link: 'https://iconify.design/docs/api/' },
+  { name: 'zodiac', provider: 'Pictogrammers', desc: 'Zodiac SVGs sourced from Pictogrammers (MDI)', link: 'https://pictogrammers.com/library/mdi/' },
+  { name: 'font', provider: 'Google', desc: 'Fonts sourced from Google Fonts', link: 'https://fonts.google.com/' },
+]
+
+export { projectCredits };
